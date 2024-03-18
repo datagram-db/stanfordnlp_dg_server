3abb91ee992e45bcf7b3094905b0147b1330b4e6
Giacomo Bergami <bergamigiacomo@gmail.com>
2024-03-18 15:49:01 +0000

    Now, starting the java service according to the Python specification

--- src/main/java/uk/ncl/giacomobergami/Main.java
+++ src/main/java/uk/ncl/giacomobergami/Main.java
@@ -1,11 +1,17 @@
 package uk.ncl.giacomobergami;
 
+import com.esotericsoftware.yamlbeans.YamlReader;
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -13,10 +19,17 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class Main {
     public static void main(String[] args) throws IOException {
         int port = 9999;
-        if (args.length > 0)
-            port = Integer.valueOf(args[0]);
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-
+        String host = "localhost";
+        if (new File("config.yaml").exists() && new File("config.yaml").isFile()) {
+            YamlReader reader = new YamlReader(new FileReader("config.yaml"));
+            Object object = reader.read();
+            if (object instanceof LinkedHashMap<?,?>) {
+                var map = (LinkedHashMap<String,String>)object;
+                port = Integer.valueOf(map.getOrDefault("stanford_nlp_port", "9999"));
+                host = map.getOrDefault("stanford_nlp_host", "localhost");
+            }
+        }
+        HttpServer server = HttpServer.create(new InetSocketAddress(InetAddress.getByName(host), port), 0);
         /*
         curl -X POST -F "name=What if I want to write something more convoluted, then?" -F "p=This is a test, continuing the former" localhost:9999/stanfordnlp
          */
